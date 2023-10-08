4408d30c290bdbd4040f4e440e699c4888be1039
vishesh-12 <vishesh121299@gmail.com>
2023-10-08 19:36:04 +0530

    first commit

---
+++ table.js
@@ -0,0 +1,17 @@
+// var num = 5;
+// for(var i = 1; i < 10; i++) {
+//     console.log(num * i)
+// }
+
+for (let index = 0; index < 3; index++) {
+    var msg = "";
+    for (let index = 1; index <= 4; index++) {
+       msg = msg + "*";
+    }
+    console.log(msg);
+}
+
+
+// ****
+// ****
+// ****
